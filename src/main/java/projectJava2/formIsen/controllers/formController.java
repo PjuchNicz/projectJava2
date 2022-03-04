699da18d59c3f07a6f077e160e1b13d46900c88b
Pierre Juchniewicz <85236071+PjuchNicz@users.noreply.github.com>
2022-03-04 16:28:51 +0100

    Forms

--- src/main/java/projectJava2/formIsen/controllers/formController.java
+++ src/main/java/projectJava2/formIsen/controllers/formController.java
@@ -4,15 +4,83 @@
 package projectJava2.formIsen.controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import projectJava2.formIsen.App;
+import projectJava2.formIsen.person.Person;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class formController {
+	@FXML
+	public TextField firstname;
+	
+	@FXML
+	public TextField lastname;
+	
+	@FXML
+	public TextField nickname;
+	
+	@FXML
+	public TextField tel;
+	
+	@FXML
+	public TextField email;
+	
+	@FXML
+	public TextField address;
+	
+	@FXML
+	public DatePicker birthdate;
 	
 	@FXML
 	public void handleLaunchButton() throws IOException {
 		// Here we make use of our new method allowing us to change views inside the main Parent		
-		//App.showView("form");
+		Person person = new Person();
+		person.setFirstname(getTextFirstname());
+		person.setLastname(getTextLastname());
+		person.setNickname(getTextNickname());
+		person.setPhone_number(getTextTel());
+		person.setEmail_address(getTextEmail());
+		person.setAddress(getTextAdress());
+		person.setBirth_date(getBirthdate());
+		System.out.print("\n" + person.getFirstname());
+		System.out.print("\n" + person.getLastname());
+		System.out.print("\n" + person.getNickname());
+		System.out.print("\n" + person.getPhone_number());
+		System.out.print("\n" + person.getEmail_address());
+		System.out.print("\n" + person.getAddress());
+		System.out.print("\n" + person.getBirth_date());
+	}
+	
+	
+	
+	public String getTextFirstname() {
+		return firstname.getText().toString();
+	}
+	
+	public String getTextLastname() {
+		return lastname.getText().toString();
+	}
+	
+	public String getTextNickname() {
+		return nickname.getText().toString();
+	}
+	
+	public String getTextTel() {
+		return tel.getText().toString();
+	}
+	
+	public String getTextEmail() {
+		return email.getText().toString();
+	}
+	
+	public String getTextAdress() {
+		return address.getText().toString();
+	}
+	
+	public LocalDate getBirthdate() {
+		return birthdate.getValue();
 	}
 }
--- src/test/java/projectJava2/formIsen/daos/PersonTestDao.java
+++ src/test/java/projectJava2/formIsen/daos/PersonTestDao.java
@@ -52,7 +52,7 @@ public class PersonTestDao {
     }
 
     @Test
-    public void shouldAddFilm() throws Exception {
+    public void shouldAddPersons() throws Exception {
         // WHEN
         Person person = new Person(null, "juch", "pierre", "pierro", "0611111111",
                 "rue","juch.pierre@",LocalDateTime.parse("2015-11-29T00:00:00.000").toLocalDate());
